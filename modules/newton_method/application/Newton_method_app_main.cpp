c9d453c4a4d7af441bbdd2ea16c76bec4d62261a
Wropwtr <buldakov.95@mail.ru>
2017-05-26 20:25:57 +0300

    Style

--- modules/newton_method/application/Newton_method_app_main.cpp
+++ modules/newton_method/application/Newton_method_app_main.cpp
@@ -7,8 +7,8 @@
 #include "include/Newton_method_app.h"
 
 int main(int argc, const char** argv) {
-	NewtonMethodApp nma;
-	std::string output = nma(argc, argv);
-	printf("%s\n", output.c_str());
-	return 0;
+    NewtonMethodApp nma;
+    std::string output = nma(argc, argv);
+    printf("%s\n", output.c_str());
+    return 0;
 }
